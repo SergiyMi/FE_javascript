70e25eea28edc4e9b3fa735f1bb65bf35f7894ec
SergiyMi <bladeneo@i.ua>
2019-06-14 10:01:17 +0300

    array and cycle

---
+++ FE_modules/module2/1/main.js
@@ -0,0 +1,22 @@
+let input;
+const numbers = [];
+let total = 0;
+input = prompt("Enter number > 0, please!");
+if (!input) {
+  alert("Cansel user!");
+} else {
+  while (input) {
+    if (Number.isNaN(Number(input))) {
+      alert("Было введено не число, попробуйте еще раз");
+    } else {
+      numbers.push(Number(input));
+    }
+    input = prompt("Enter number > 0, please!");
+  }
+}
+if (numbers.length > 0) {
+  for (let i = 0; i < numbers.length; i++) {
+    total = total + numbers[i];
+  }
+  console.log(`Общая сумма чисел равна ${total}`);
+}
---
+++ FE_modules/module2/2/main.js
@@ -0,0 +1,21 @@
+const passwords = ["qwerty", "111qwe", "123123", "r4nd0mp4zzw0rd"];
+let attemptsLeft = 3;
+let inputUser;
+while(attemptsLeft > 0){
+    inputUser = prompt("Enter the password!");
+    if (!inputUser) {
+        alert('Cansel');
+        break;
+    } else if (passwords.includes(inputUser)){
+        alert('Добро пожаловать!');
+        break;
+    } else {
+        attemptsLeft -= 1;
+        if (attemptsLeft > 0){
+            alert(`Неверный пароль, у вас осталось ${attemptsLeft} попыток`);
+        } else {
+            alert('У вас закончились попытки, аккаунт заблокирован!');
+            break;
+        }
+    }
+}
